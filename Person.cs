7a8f1244aebc167c09aa6883408a8dbaedccaa51
habib mbow <hhmbow@hotmail.com>
2024-03-11 09:38:19 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning3
+{
+  
+        public class Person
+        {
+
+            private int _Age;
+            private string _FName;
+            private string _LName;
+            private int _Height;
+            private int _Weight;
+
+            public int age
+            {
+                get => _Age;
+                set {
+                _Age = value;
+            }
+            }
+            public string fname
+            {
+                get => _FName;
+                set { 
+                _FName = value;
+            }
+            }
+            public string lname
+            {
+                get => _LName;
+                set {
+                _LName = value;
+            }
+            }
+            public int height
+            {
+                get => _Height;
+                set { 
+                _Height = value;
+            }
+            }
+            public int weight
+            {
+                get => _Weight;
+                set { 
+                _Weight = value;
+            }
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Övning3;
+using System;
+
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.age = 7;
+        person.fname = "Sidi";
+        person.lname = "Fall";
+        person.height = 190;
+        person.weight = 70;
+
+        Console.WriteLine($"Hey my fullname is: {person.fname + " " + person.lname}, my age is: {person.age}, my height is: {person.height}, and my weight is: {person.weight} ");
+
+    }
+}
+
