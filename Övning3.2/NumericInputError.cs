2bd24913f45cc426fd40c362bc99963ead3b8040
habib mbow <hhmbow@hotmail.com>
2024-03-18 16:58:38 +0100

    updates

--- Övning3.2/NumericInputError.cs
+++ Övning3.2/NumericInputError.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Övning3._2
+﻿
+public class NumericInputError : UserError
 {
-    public class NumericInputError : UserError
+    public override string UEMessage()
     {
-        public override string UEMessage()
-        {
-            return "You tried to use a numeric input in a text-only field. This fired an error!";
-        }
+        return "You tried to use a numeric input in a text only field. This fired an error!";
     }
 }
